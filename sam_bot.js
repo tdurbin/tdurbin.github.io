660b43051775e4d569f7a363b8ade73a65f909c6
tdurbin <tdurbin@liveperson.com>
2020-02-18 14:14:00 +0000

    commit

---
+++ sam_bot.js
@@ -0,0 +1,17 @@
+lpTag.sdes.push({
+    "type": "ctmrinfo",
+    "info": {
+        customerId: '1'
+    }
+});
+
+// Authentication JSMethod for LiveEngage
+var lpGetAuthenticationToken = function(callback) {
+    console.log("inside lpGetAuthenticationToken!");
+    var token = "sam_bot";
+    if (token) {
+        console.log("got token! " + token);
+        console.log("calling callback with token...");
+        callback(token);
+    }
+};
