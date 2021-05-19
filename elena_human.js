f323e09375adecce878eafd41f4cc27943923a0b
tdurbin <tdurbin@liveperson.com>
2021-05-19 08:15:41 +0100

    commit

---
+++ elena_human.js
@@ -0,0 +1,19 @@
+var lpGetAuthenticationToken = function (callback) {
+    console.log("inside lpGetAuthenticationToken!");
+    var token = "elena_human";
+    if (token) {
+        console.log("got token! " + token);
+        console.log("calling callback with token...");
+        callback(token);
+    }
+};
+
+lpTag.identities = [];
+lpTag.identities.push(identityFn);
+function identityFn(callback) {
+    callback({
+        iss: "https://twd-oauth2.herokuapp.com/",
+        acr: "loa1",
+        sub: "enatke|20210519"
+    });
+}
