f457e5c28859aca389151057c3add4b67291145f
tdurbin <tdurbin@liveperson.com>
2022-10-24 12:17:19 +0100

    commit

---
+++ loan.js
@@ -0,0 +1,19 @@
+lpTag.sdes = lpTag.sdes||[];
+lpTag.sdes.push(
+   {
+        "type": "purchase",
+        "total": 15000,
+        "currency": "GBP",
+        "orderId": "HPL153454",
+        "cart":{
+            "products": [{
+                "product": {
+                "name": "Premier Personal Loan",
+                "category": "Loan",
+                "sku": "hsbc_prem_loan",
+                "price": 15000
+                }, "quantity": 1
+            }]
+        }
+   }
+);
