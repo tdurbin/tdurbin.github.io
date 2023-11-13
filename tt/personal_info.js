d86f7a1b7a8f27161952990d5cf2dec59d900ca5
tdurbin <tdurbin@liveperson.com>
2023-11-13 13:55:18 +0000

    commit

--- tt/personal_info.js
+++ tt/personal_info.js
@@ -9,16 +9,16 @@ lpTag.sdes.push(
                 "age": 49,  // AGE AS INTEGER
                 "year": 1974,  // BIRTH YEAR
                 "month": 7,  // BIRTH MONTH
-                "day": 30  // BIRTH DAY
+                "day": 27  // BIRTH DAY
             },
             "contacts": [{
                 "email": "tom.durbin@me.com",  // EMAIL
                 "phone": "+447766367842",  // PHONE NUMBER
                 "preferred": "EMAIL",  // EMAIL, PHONE
                 "address": {
-                    "country": "UK"  // COUNTRY
-                   "region": "OXON"  // REGION
-                   "zipcode": "OX26 6xa"  // ZIP OR POSTAL CODE
+                    "country": "UK",  // COUNTRY
+                    "region": "OXON",  // REGION
+                    "zipcode": "OX26 6XA"  // ZIP OR POSTAL CODE
                 },
             }],
             "gender": "MALE",  // MALE, FEMALE, OTHER
