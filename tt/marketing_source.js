50313a38bb9f0e64822fff1f483bbb9dc90aaa98
tdurbin <tdurbin@liveperson.com>
2023-11-14 10:37:42 +0000

    commit

---
+++ tt/marketing_source.js
@@ -0,0 +1,12 @@
+lpTag.sdes = lpTag.sdes||[];
+lpTag.sdes.push(
+   {
+        "type": "mrktInfo",  //MANDATORY
+            "info": {
+            "channel": "1",  //ORIGINATING CHANNEL ENUM:
+             // 0-Direct, 1-Search, 2-Social, 3-Email, 4-Referral, 5-Paid Search, 6-Display
+            "affiliate": "Google",  //AFFILIATE NAME
+            "campaignId": "Full Fibre Search Campaign"  //EXTERNAL ORIGINATING CAMPAIGN 
+        }
+   }
+);
