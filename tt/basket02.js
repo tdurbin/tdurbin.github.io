2f00eea2049fac75c3938295c410e9a280e333e2
tdurbin <tdurbin@liveperson.com>
2023-11-14 11:59:34 +0000

    commit

---
+++ tt/basket02.js
@@ -0,0 +1,33 @@
+lpTag.sdes = lpTag.sdes||[];
+lpTag.sdes.push(
+   {
+        "type": "cart",
+        "total": 810,
+        "currency": "GBP",
+        "numItems": 3,
+        "products": [{
+            "product": {
+            "name": "Full Fibre 900",
+            "category": "Full Fibre",
+            "sku": "ff900",
+            "price": 45
+            }, "quantity": 1
+        },
+        {
+            "product": {
+            "name": "Total Home Wi-Fi",
+            "category": "Wi-Fi",
+            "sku": "totalwifi",
+            "price": 0
+            }, "quantity": 1
+        },
+        {
+            "product": {
+            "name": "Amazon eero pro 6",
+            "category": "Router",
+            "sku": "eeropro6",
+            "price": 0
+            }, "quantity": 2
+        }]
+   }
+);
